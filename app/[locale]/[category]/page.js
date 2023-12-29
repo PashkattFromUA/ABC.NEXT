5edc429e443676a657fc990d5532e719b9a46238
PashkattFromUA <pashkattfromua@gmail.com>
2023-12-29 12:20:14 +0200

    static props for category page

--- app/[locale]/[category]/page.js
+++ app/[locale]/[category]/page.js
@@ -8,6 +8,13 @@ import { notFound } from 'next/navigation'
 
 const i18nNamespaces = ['common'];
 
+export async function generateStaticParams() {
+  const labels = await fetch('https://api.abcrypto.io/api/sitemap').then(res => res.json());
+  const catnames = labels.data.filter(item => item.type === 'category');
+  const catslugArray = catnames.map(item => ({slug: item.data.slug,}));
+  return catslugArray.map(item => ({ category: item.slug}));
+}
+
 async function getCards(props) {
   const url = `https://api.abcrypto.io/api/categories/${props.catslug}/items/all`;
   const headers = new Headers({

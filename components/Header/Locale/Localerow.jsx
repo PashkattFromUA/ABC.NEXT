8b68aa2e9bc60bd520634989fe84430ec6d3be1d
PashkattFromUA <pashkattfromua@gmail.com>
2024-01-05 13:15:21 +0200

    bug fixing

--- components/Header/Locale/Localerow.jsx
+++ components/Header/Locale/Localerow.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import { useTranslation } from 'next-i18next';
+import { useTranslation } from 'react-i18next';
 import styles from '@/styles/localerow.module.css'
 
 const Localerow = (props) => {

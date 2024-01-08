23b9d1df2452533888bd6fbc1dde4103e90ed600
PashkattFromUA <pashkattfromua@gmail.com>
2024-01-08 12:32:07 +0200

    meta setup for category page

--- app/[locale]/[category]/layout.js
+++ app/[locale]/[category]/layout.js
@@ -1,7 +1,3 @@
-import initTranslations from '../../i18n';
-
-const i18nNamespaces = ['common'];
-
 export async function generateStaticParams() {
     const labels = await fetch('https://api.abcrypto.io/api/sitemap').then(res => res.json());
     const catnames = labels.data.filter(item => item.type === 'category');
@@ -10,14 +6,10 @@ export async function generateStaticParams() {
 }
 
 export async function generateMetadata({ params }) {
-    const locale = params.locale;
     const catname = params.category;
-    const { t } = await initTranslations(locale, i18nNamespaces);
     const mainurl = "https://abcrypto.io";
   
     return {
-      title: `${t('titlemain')}`,
-      description: `${t('metamain')}`,
       alternates: {
         canonical: `${mainurl}/${catname}`,
         languages: {
--- app/[locale]/[category]/page.js
+++ app/[locale]/[category]/page.js
@@ -34,6 +34,21 @@ async function getLabels(lang) {
   return res.json();
 }
 
+export async function generateMetadata({ params }) {
+  const locale = params.locale;
+  const catslug = params.category;
+  const { t } = await initTranslations(locale, i18nNamespaces);
+  const labels = await getLabels(locale);
+  var resultObject = labels.data.find(function (item) {
+    return item.slug === catslug;
+  });
+
+  return {
+    title: `${t('titlecategoryleft')}${resultObject.name}${t('titlecategoryright')}`,
+    description: `${t('metacategoryleft')}${resultObject.name}${t('metacategoryright')}`
+  }
+}
+
 export default async function CategoryPage({ params }) {
 
   const locale = params.locale;

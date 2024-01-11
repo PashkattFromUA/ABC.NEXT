8cc702ea454fefc91cbd750e0cf6b725055728a6
PashkattFromUA <pashkattfromua@gmail.com>
2024-01-11 11:54:13 +0200

    added link rel to newspage

--- app/[locale]/news/page.js
+++ app/[locale]/news/page.js
@@ -1,5 +1,20 @@
 import Newsblock from "@/components/Newsblock/Newsblock"
 
+export async function generateMetadata({ params }) {
+
+    const mainurl = "https://abcrypto.io";
+
+    return {
+        alternates: {
+            canonical: `${mainurl}/news`,
+            languages: {
+                'ru': `${mainurl}/ru/news`,
+                'uk': `${mainurl}/uk/news`
+            }
+        }
+    }
+}
+
 const Newspage = () => {
 
 
